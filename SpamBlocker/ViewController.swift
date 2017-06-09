3a7dc908d4cc07dfd4429584fddf014a735c8026
Marcel Coldewey <marcel.coldewey@msp-hh-nb1608.local>
2017-06-09 17:21:39 +0200

    Initial Commit

---
+++ SpamBlocker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpamBlocker
+//
+//  Created by Marcel Coldewey | MSP AG on 09.06.17.
+//  Copyright © 2017 dev.it-coldewey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
